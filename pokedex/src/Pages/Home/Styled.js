87f5b5c4145f2ceefa39fb040725b75f92022678
Bruna Vitória Oliveira de Sousa <brunavitoria@msn.com>
2022-07-22 21:04:49 -0300

    Ajeitando responsividade da tela inicial

--- pokedex/src/Pages/Home/Styled.js
+++ pokedex/src/Pages/Home/Styled.js
@@ -13,6 +13,9 @@ margin-bottom: 2px;
 @media (max-width: 970px){
     text-align: center;
 }
+@media screen and (min-device-width : 320px) and (max-device-width : 480px) {
+font-size: 30px;
+}
 `
 
 export const DisplayCards = styled.div`
